5846f3cd3a32a15b10953ea9db21012c4b554a93
tianqiyuan520 <1540131499@qq.com>
2022-09-24 20:19:41 +0800

    commit info

--- updata.bat
+++ updata.bat
@@ -1,5 +1,5 @@
 git add .
-git remove updata.bat
+git rm updata.bat
 git commit -m "commit info"
 git remote add origin https://github.com/tianqiyuan520/tianqiyuan520.github.io.git
 git push -u origin master
